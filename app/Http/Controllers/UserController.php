d4af3cccfb78ec784ce6af8296868306af9b31e5
zxcghoul223 <k.kakashi123123@gmail.com>
2026-04-02 01:16:06 +0600

    dodelali

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class BladeTaskController extends Controller
+class UserController extends Controller
 {
     public function n1()
     {
